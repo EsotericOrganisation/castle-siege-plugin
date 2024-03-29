96584a634bf2d857839f97f57d26601d3b5da79d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-29 22:26:21 +0000

    Initial commit

---
+++ src/main/java/net/slqmy/castle_siege_plugin/CastleSiegePlugin.java
@@ -0,0 +1,17 @@
+package net.slqmy.castle_siege_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CastleSiegePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
